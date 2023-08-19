0f1d11328d640c1b871b486ecd0059ca3218ac62
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-08-19 10:17:18 -0500

    fix: Set google auth prompt to consent

--- src/routers/google/services/generateAuthUrl.ts
+++ src/routers/google/services/generateAuthUrl.ts
@@ -6,6 +6,7 @@ export const generateAuthUrl = (redirectUrl: string) => {
       "https://www.googleapis.com/auth/userinfo.profile",
       "https://www.googleapis.com/auth/userinfo.email",
     ],
+    prompt: "consent",
     access_type: "offline",
     state: JSON.stringify({ redirectUrl }),
   });
